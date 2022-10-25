fc583cae745ee62e40e953d5204e47c9912af6f9
Zin Zin Thin Zaw <60064474+zinzinthinzaw9@users.noreply.github.com>
2022-10-25 11:14:02 +0630

    add config file

--- admin/config/config.php
+++ admin/config/config.php
@@ -1,1 +1,12 @@
+<?php
+  $dbhost = "localhost";
+  $dbuser = "root";
+  $dbpass = "";
+  $dbname = "bookstorethird";
 
+  $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+  // if ($conn) {
+  // 	echo "success connection";
+  // }
+  
+?>
